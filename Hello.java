c68ff0e08575171b3cd6e52fece5731157b0b483
Nischal Shrestha <sthanischal5060@gmail.com>
2026-04-11 21:00:04 +0545

    class task 1

--- Hello.java
+++ Hello.java
@@ -1,7 +1,29 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("hello \\ broooooooooooo");
-        String var="Hello broo";
-        System.out.println(var);
+        // System.out.println("Hello World!");
+        // System.out.print("This is my first program");
+        // System.out.print("I am on module ST4003CEM");
+        // System.out.println("A \"quoted\" String is");
+        // System.out.println("'much' better if you learn \nthe rules of \"escape sequences.\"");
+        // System.out.println("Also, \"\" represents an empty");
+        // System.out.println("String. Don't forget: use \\\"");
+        // System.out.println("instead of \"!");
+        // System.out.println("\"is not the same as \"");
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+        // System.out.println("*****");
+
+        // for (int i=0;i<5;i++){
+        //     System.out.println("*********");
+        // }
+        System.out.println("A well-formed Java program has a main method with { and } braces. \n");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that");
+        System.out.println("starts and ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"? \n");
+        System.out.println("One is what we see when we're typing our program. The other is what appears on \nthe \"console.\"");
     }
 }

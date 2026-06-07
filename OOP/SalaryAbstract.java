f2ecacda37c0eda86337e85ed77bf76500eef390
Nischal Shrestha <sthanischal5060@gmail.com>
2026-06-08 05:29:29 +0545

    salary abstraction

--- OOP/SalaryAbstract.java
+++ OOP/SalaryAbstract.java
@@ -1,39 +1,43 @@
-abstract class Employee{
+abstract class Employee {
+
     String e_name;
+
     abstract double calculateSalary();
 }
 
-class FullTime extends Employee{
+class FullTime extends Employee {
+
     @Override
-    double calculateSalary(){
+    double calculateSalary() {
         return 30000;
     }
 }
-class Part_Time extends Employee{
+
+class Part_Time extends Employee {
+
     private double hour;
     private double rate;
 
-    Part_Time(double hour,double rate){
-        this.hour=hour;
-        this.rate=rate;
+    Part_Time(double hour, double rate) {
+        this.hour = hour;
+        this.rate = rate;
     }
 
     @Override
-    double calculateSalary(){
-        
-        return hour*rate;
+    double calculateSalary() {
+        return hour * rate;
     }
 }
 
-public class SalaryAbstract{
-    public static void main(String [] args){
-        Employee e1=new FullTime();
-        e1.e_name="Samir";
+public class SalaryAbstract {
+
+    public static void main(String[] args) {
+        Employee e1 = new FullTime();
+        e1.e_name = "Samir";
         System.out.println(e1.calculateSalary());
 
-        Employee e2=new Part_Time(24, 40);
-        e2.e_name="Nischal";
-       System.out.println(e2.calculateSalary());
+        Employee e2 = new Part_Time(24, 40);
+        e2.e_name = "Nischal";
+        System.out.println(e2.calculateSalary());
     }
 }
-  

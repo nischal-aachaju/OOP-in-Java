14d8f2f5535306f09400bf3bced8931116b47779
Nischal Shrestha <sthanischal5060@gmail.com>
2026-06-08 12:38:08 +0545

    polymorphism

---
+++ OOP/MainCal.java
@@ -0,0 +1,22 @@
+class CalculationExample{
+     
+    int sum(int a, int b){
+        return a+b;
+    }
+    int sum(int a,int b,int c){
+        return a+b+c;
+    }
+    
+    double sum(double a,int b){
+        return a+b;
+    }
+}
+
+class MainCal{
+    public static void main(String [] args){
+        CalculationExample cal=new CalculationExample();
+        System.out.println(cal.sum(54,34,34));
+        System.out.println(cal.sum(34,34));
+        System.out.println(cal.sum(54,34,34));
+    }
+}

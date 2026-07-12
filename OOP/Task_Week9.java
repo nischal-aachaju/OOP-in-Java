409fa3f0e96a3effd517776eb13d7dbede5a3850
Nischal Shrestha <sthanischal5060@gmail.com>
2026-07-12 15:07:57 +0545

    java practice

--- OOP/Task_Week9.java
+++ OOP/Task_Week9.java
@@ -1,19 +1,26 @@
 class Shape{
     public void calculateArea(){
-        System.out.println("Area");
+        System.out.println("Area of shape");
     }
 }
 
 class Circle extends Shape{
     public void calculateCircumference(){
-        System.out.println("Circumference");
+        System.out.println("Circumference of Circle");
     }
 }
 
 class Rectangle extends Shape{
     public void calculatePerimeter(){
-        System.out.println("Perimeter");
+        System.out.println("Perimeter of Rectangle");
     }
+}
+
+class Square extends Shape{
+    public void calculatePerimeter(){
+        System.out.println("Perimiter of square");
+    }
+
 }
 
 class MainShape{
@@ -25,6 +32,10 @@ class MainShape{
         Rectangle r1=new Rectangle();
         r1.calculateArea();
         r1.calculatePerimeter();
+
+        Square s1=new Square();
+        s1.calculateArea();
+        s1.calculatePerimeter();
     }
 
 }
